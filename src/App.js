cc77d331f888a477224347e92db0411377df1937
TobiTgl <tobias_813@t-online.de>
2022-09-24 00:05:08 +0200

    mobile

--- src/App.js
+++ src/App.js
@@ -9,14 +9,28 @@ function App() {
   
   const html = document.documentElement;
   const context = canvas.getContext("2d")
-  
+  let ratio = window.innerWidth / window.innerHeight
   const frameCount = 1120;
-  const currentFrame = index => (
-    `https://res.cloudinary.com/tobitgl/image/upload/v1663511212/Portfolio/CoffeTable/ersteFinal${index.toString().padStart(4, '0')}.webp`
+  const currentFrame = index =>(
+    webOrMobile(index)
   )
+//  const currentFrame = index => (
+//    `https://res.cloudinary.com/tobitgl/image/upload/v1663511212/Portfolio/CoffeTable/mobil/mobile${index.toString().padStart(4, '0')}.webp`
+//  )
+//  const currentFrameMobile = index => (
+//    `https://res.cloudinary.com/tobitgl/image/upload/v1663511212/Portfolio/CoffeTable/mobile${index.toString().padStart(4, '0')}.webp`
+//  )
+const webOrMobile = (index) =>{
+  console.log(ratio)
+  if(ratio > 1){
+      return `https://res.cloudinary.com/tobitgl/image/upload/v1663511212/Portfolio/CoffeTable/ersteFinal${index.toString().padStart(4, '0')}.webp`
+  }else{
+      return  `https://res.cloudinary.com/tobitgl/image/upload/v1663511212/Portfolio/CoffeTable/mobil/mobile${index.toString().padStart(4, '0')}.webp`
+  }
+}
   
   const preloadImages = () => {
-    console.log('preload')
+    console.log(window.innerWidth)
     for (let i = 1; i < frameCount; i++) {
       const img = new Image();
       img.src = currentFrame(i);
@@ -25,8 +39,13 @@ function App() {
   
   const img = new Image()
   img.src = currentFrame(1);
-  canvas.width=1920;
-  canvas.height=1080;
+  if(ratio > 1){
+    canvas.width=1920;
+    canvas.height=1080;
+  }else{
+  canvas.width=1080;
+  canvas.height=1920;
+  }
   img.onload=function(){
     context.drawImage(img, 0, 0);
   }
@@ -44,14 +63,10 @@ function App() {
     const frameIndex = Math.min(
       frameCount - 1,
       mathCeil
-    );
-    console.log(window.innerHeight)
-    console.log(window.innerWidth)
-
-    
+    );    
     requestAnimationFrame(() => updateImage(frameIndex + 1))
   });
-  
+
   preloadImages()
   
   return (
